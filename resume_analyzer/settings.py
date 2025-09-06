c72ad63fecff4e7037bf033b3cde2028c838e985
kish7867 <jaiswalkishan577@gmail.com>
2025-09-06 20:14:32 +0530

    Fixing minor error

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY ='django-insecure-g2_=dr4xbwxtbs*ce)7s^hxjaa(0x*625!(bcuy7cm4-+x@)*e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
