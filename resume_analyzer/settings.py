9401cd2005eee1a9bb5e2164041ff77c32768e69
kish7867 <jaiswalkishan577@gmail.com>
2025-10-11 07:18:36 +0530

    Updated database configuration to use Aiven PostgreSQL

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -88,8 +88,7 @@ WSGI_APPLICATION = 'resume_analyzer.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
